933db428983176db8540977c0ca778cae840c198
Domestos002 <ENbLEiuPm74p2946333314>
2016-12-23 19:31:43 +0600

    gfdcjuykfuckgh

--- app/js/app.js
+++ app/js/app.js
@@ -1,142 +1,54 @@
-(function() {
-
-    function YOURAPPNAME(doc) {
-        var _self = this;
-
-        _self.doc = doc;
-        _self.window = window;
-
-        _self.bootstrap();
+;(function () {
+    function num() {
+        var i = 1;
+        $(".b-advantages-markered-list").find(".item__number").each(function () {
+            $(this).append(i);
+            i++;
+        });
     }
+    $(document).ready(function () {
+        $(".slider-range").each(function () {
+            var slider = $(this);
+            slider.slider();
+            /*slider.on("slide", function (slideEvt) {
+             slider.closest('.slider-block').find('[data-input-value]').val(slideEvt.value);
+             });
+             setTimeout(function () {
+             slider.closest('.slider-block').find('[data-input-value]').val(slider.data('slider').value[0]);
+             }, 500);*/
+        });
 
-    YOURAPPNAME.prototype.bootstrap = function() {
-        var _self = this;
-
-    };
-
-    // Window load types (loading, dom, full)
-    YOURAPPNAME.prototype.appLoad  = function (type, callback) {
-        var _self = this;
-
-        switch(type) {
-            case 'loading':
-                if (_self.doc.readyState === 'loading') callback();
-
-                break;
-            case 'dom':
-                _self.doc.onreadystatechange = function () {
-                    if (_self.doc.readyState === 'complete') callback();
-                };
-
-                break;
-            case 'full':
-                _self.window.onload = function(e) {
-                    callback(e);
-                };
-
-                break;
-            default:
-                callback();
+        if ($("select, input:radio").length > 0) {
+            $("select, input:radio").styler();
         }
-    };
 
-    YOURAPPNAME.prototype.initSwitcher = function () {
-        var _self = this;
+        $(".b-products-item").click(function () {
+            $(this).find("#prod-item-link").trigger("click");
+        });
+    });
 
-        var switchers = _self.doc.querySelectorAll('[data-switcher]');
+    $(function () {
+        num();
+        $('a.js-scrollScreen[href*="#"]:not([href="#"])').click(function () {
+            if ($(this).hasClass('sliding-panel-link')) {
+                $('body').removeClass('ovh');
+                $('.sliding-panel').removeClass('active');
+                $('.site-wrapper-content').removeClass('active');
+                $(".site-wrapper").css("width", "auto");
+                $(".site-wrapper").toggleClass("ovh");
 
-        if(switchers && switchers.length > 0) {
-            for(var i=0; i<switchers.length; i++) {
-                var switcher = switchers[i],
-                    switcherOptions = _self.options(switcher.dataset.switcher),
-                    switcherElems = switcher.children,
-                    switcherTargets = _self.doc.querySelector('[data-switcher-target="' + switcherOptions.target + '"]').children;
-
-                for(var y=0; y<switcherElems.length; y++) {
-                    var switcherElem = switcherElems[y],
-                        parentNode = switcher.children,
-                        switcherTarget = switcherTargets[y];
-
-                    if(switcherElem.classList.contains('active')) {
-                        for(var z=0; z<parentNode.length; z++) {
-                            parentNode[z].classList.remove('active');
-                            switcherTargets[z].classList.remove('active');
-                        }
-                        switcherElem.classList.add('active');
-                        switcherTarget.classList.add('active');
-                    }
-
-                    switcherElem.children[0].addEventListener('click', function (elem, target, parent, targets) {
-                        return function (e) {
-                            e.preventDefault();
-                            if(!elem.classList.contains('active')) {
-                                for(var z=0; z<parentNode.length; z++) {
-                                    parent[z].classList.remove('active');
-                                    targets[z].classList.remove('active');
-                                }
-                                elem.classList.add('active');
-                                target.classList.add('active');
-                            }
-                        };
-
-                    }(switcherElem, switcherTarget, parentNode, switcherTargets));
+            }
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[id=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
                 }
             }
-        }
-    };
-
-    YOURAPPNAME.prototype.str2json = function(str, notevil) {
-        try {
-            if (notevil) {
-                return JSON.parse(str
-                    .replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":';})
-                    .replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"';})
-                );
-            } else {
-                return (new Function("", "var json = " + str + "; return JSON.parse(JSON.stringify(json));"))();
-            }
-        } catch(e) { return false; }
-    };
-
-    YOURAPPNAME.prototype.options = function(string) {
-        var _self = this;
-
-        if (typeof string !='string') return string;
-
-        if (string.indexOf(':') != -1 && string.trim().substr(-1) != '}') {
-            string = '{'+string+'}';
-        }
-
-        var start = (string ? string.indexOf("{") : -1), options = {};
-
-        if (start != -1) {
-            try {
-                options = _self.str2json(string.substr(start));
-            } catch (e) {}
-        }
-
-        return options;
-    };
-
-    var app = new YOURAPPNAME(document);
-
-    app.appLoad('loading', function () {
-        console.log('App is loading... Paste your app code here.');
-        // App is loading... Paste your app code here. 4example u can run preloader event here and stop it in action appLoad dom or full
+        });
     });
-
-    app.appLoad('dom', function () {
-        console.log('DOM is loaded! Paste your app code here (Pure JS code).');
-        // DOM is loaded! Paste your app code here (Pure JS code).
-        // Do not use jQuery here cause external libs do not loads here...
-
-        app.initSwitcher(); // data-switcher="{target='anything'}" , data-switcher-target="anything"
-    });
-
-    app.appLoad('full', function (e) {
-        console.log('App was fully load! Paste external app source code here... For example if your use jQuery and something else');
-        // App was fully load! Paste external app source code here... 4example if your use jQuery and something else
-        // Please do not use jQuery ready state function to avoid mass calling document event trigger!
-    });
-
 })();
+
